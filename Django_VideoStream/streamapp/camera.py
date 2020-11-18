e4df1cd0c0d84131eff815302228b3f2c497cceb
PawanKP09 <59362224+PawanKP09@users.noreply.github.com>
2020-11-18 14:59:34 +0530

    Updated camera.py(Django_VideoStream)

--- Django_VideoStream/streamapp/camera.py
+++ Django_VideoStream/streamapp/camera.py
@@ -13,14 +13,28 @@ face_detection_webcam = cv2.CascadeClassifier(os.path.join(
 url = "http://127.0.0.1:8000/accounts/process/"
 
 
+# Defining configs
+TOLERANCE = 0.6
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+
+
 class VideoCamera(object):
 	def __init__(self):
 		self.video = cv2.VideoCapture(0)
 
 	def __del__(self):
 		self.video.release()
+		
+	# Returns (R, G, B) from name
+	def name_to_color(name):
+		# Take 3 first letters, tolower()
+		# lowercased character ord() value rage is 97 to 122, substract 97, multiply by 8
+		color = [(ord(c.lower())-97)*8 for c in name[:3]]
+		return color
 
 	def get_frame(self):
+	
 		print("Loading the encoded faces from the saved file!!!")
 		# Load face encodings from saved files.
 		# with open('dataset_faces_v2.1.dat', 'rb') as f:
@@ -64,9 +78,9 @@ class VideoCamera(object):
 			print("\r", end='')
 			print("{}".format(no_face), end="", sep=" ", flush=True)
 
-
 		elif len(face_locations) > 1:
 			print("Multiple Faces detected")
+			
 		else:
 			# Encode faces from the live input
 			face_encodings = face_recognition.face_encodings(face_picture, face_locations)
@@ -81,6 +95,7 @@ class VideoCamera(object):
 
 				# See if the face is a match for the known face (that we saved in the precedent step)
 				matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+				match = None
 
 				if True in matches:
 					# print("Known face in the frame")
@@ -105,11 +120,35 @@ class VideoCamera(object):
 								   'Accept': 'text/plain'}
 						post_data = {"name": str(name)}
 						print(post_data)
+						
+						# Drawing box around the faces
+						top_left = (face_locations[0][3], face_locations[0][0])
+						bottom_right = (face_locations[0][1], face_locations[0][2])
+
+						# Get color by name using our fancy function
+						color = [0, 255, 0]
+						
+
+						# Paint frame
+						cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+						# Drawing box for name below the detected face.
+						top_left = (face_locations[0][3], face_locations[0][2])
+						bottom_right = (face_locations[0][1], face_locations[0][2] + 22)
+						
+						# Paint frame
+						cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
 
 						check = requests.post(url, data=json.dumps(post_data),
 											  headers=headers)
+						
+						# Wite name on the deteced faces
+						cv2.putText(image, name, (face_locations[0][3] + 10, face_locations[0][2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), FONT_THICKNESS)
+						
+						"""
 						print(check)
 						cv2.putText(face_picture, str(name), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+						"""
 
 					else:
 						print("\r", end='')
@@ -117,10 +156,7 @@ class VideoCamera(object):
 
 		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 		faces_detected = face_detection_videocam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-		for (x, y, w, h) in faces_detected:
-			cv2.rectangle(image, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-		frame_flip = cv2.flip(image,1)
-		ret, jpeg = cv2.imencode('.jpg', frame_flip)
+		ret, jpeg = cv2.imencode('.jpg', image)
 		return jpeg.tobytes()
 
 
